b2dcdd57ebe430e8565bd804d2c77260c630b415
Abinash <Abinashpanda393@gmail.com>
2019-12-07 00:53:26 +0530

    fonts-added

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom'
 
 // importing components
 import Header from './components/Header'
-// import Main from './components/Main'
+import Main from './components/Main'
 
-// importing sass & stylesheets
+// importing sass & stylesheets & fonts
 import './sass/index.scss'
 
 function App() {
     return(
-        <>
+        <> 
             <Header/>
-            {/* <Main/> */}
+            <Main/>
         </>
     )
 }
